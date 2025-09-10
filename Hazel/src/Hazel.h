adc04de42d18b17185679b43055767aa9a00c909
yhyu <773191556@qq.com>
2025-09-10 12:22:24 +0800

    premake done!

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use by Yhyu applications
+#include "Hazel/Application.h"
+#include "Hazel/Log.h"
+//---Entry points----------------------
+
+#include "Hazel/EntryPoint.h"
+
+//-------------------------------------
